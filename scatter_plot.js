d86cd8cd3521f16a112c5fba67cf7b6e9444162f
whao89 <whao@princeton.edu>
2016-04-20 12:39:52 -0400

    FOV

--- scatter_plot.js
+++ scatter_plot.js
@@ -13,8 +13,8 @@ function scatterPlot3d( parent )
   var scene = x3d.append("scene")
 
   scene.append("orthoviewpoint")
-     .attr( "centerOfRotation", [0, 0, 0])
-     .attr( "fieldOfView", [-5, -5, 8, 8])
+     .attr( "centerOfRotation", [4, 4, 4])
+     .attr( "fieldOfView", [-5, -5, 13, 13])
      .attr( "orientation", [-0.5, 1, 0.2, 1.12*Math.PI/4])
      .attr( "position", [8, 4, 15])
 
